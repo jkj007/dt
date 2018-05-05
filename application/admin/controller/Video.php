fc2fe81400316d560b5b367afdfe14a105f79f03
pemon <931692760@qq.com>
2018-05-06 00:00:12 +0800

    Revert "视频上传,添加成功,待数据验证"
    This reverts commit 7f4ee6b5577ebb919e09dfc5f8e4237fba15c0fe.

--- application/admin/controller/Video.php
+++ application/admin/controller/Video.php
@@ -1,12 +1,11 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
-use think\Request;
 use app\admin\model\Video as M;
 class Video extends Controller
 {
   public function index(){
-    $info = M::order('create_time desc')->paginate(15);
+    $info = M::paginate(15);
     return $this->fetch('index',compact('info'));
   }
   public function create()
@@ -14,12 +13,10 @@ class Video extends Controller
     return $this->fetch();
   }
 
-  public function save(Request $request)
+  public function save()
   {
-      $video = new M(input('post.'));
-      // 过滤post数组中的非数据表字段数据
-      $video->allowField(true)->save();
-      return $this->redirect('/avideo','',302,['code'=>0,'msg'=>'添加成功']);
+    dump(input('post.'));
+    return 111;
   }
 
 }
