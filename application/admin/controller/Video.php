93044b6bc3b5ce83847c569066a4a3d2822caf83
pemon <931692760@qq.com>
2018-05-07 00:25:37 +0800

    验证完成

--- application/admin/controller/Video.php
+++ application/admin/controller/Video.php
@@ -1,25 +1,47 @@
 <?php
 namespace app\admin\controller;
+
 use think\Controller;
 use think\Request;
 use app\admin\model\Video as M;
+
 class Video extends Controller
 {
-  public function index(){
-    $info = M::order('create_time desc')->paginate(15);
-    return $this->fetch('index',compact('info'));
-  }
-  public function create()
-  {
-    return $this->fetch();
-  }
+    public function index(){
+        $info = M::order('create_time desc')->paginate(15);
+        return $this->fetch('index',compact('info'));
+    }
+    public function create()
+    {
+        return $this->fetch();
+    }
 
-  public function save(Request $request)
-  {
-      $video = new M(input('post.'));
-      // 过滤post数组中的非数据表字段数据
-      $video->allowField(true)->save();
-      return $this->redirect('/avideo','',302,['code'=>0,'msg'=>'添加成功']);
-  }
+    public function save(Request $request)
+    {
+      $video = new M();
+
+
+        $result = $video
+                    ->allowField(true)// 过滤post数组中的非数据表字段数据
+                    ->validate([
+                            'title' =>'require|max:255',
+                            'video' =>'require',
+                            'type'  =>'require',
+                            'price' =>'require|number ',
+                        ],[
+                            'title.require'=>'标题必填',
+                            'title.max'=>'标题最大长度255',
+                            'video.require'=>'视频为必填项',
+                            'type.require'=>'类型为必填项',
+                            'price.require'=>'价格为必填项',
+                            'price.number'=>'价格必须为数字',
+                        ],true)
+                    ->save(input('post.'));
+        if (false === $result) {
+        $error = $video->getError();
+            return $this->redirect('/avideo/create','',302,$error);
+        }
+        return $this->redirect('/avideo','',302,['code'=>0,'msg'=>'添加成功']);
+    }
 
 }
---
+++ public/static/pm/file_upload.js
@@ -0,0 +1,105 @@
+$(function () {
+    'use strict';
+    // Change this to the location of your server-side upload handler:
+    var url = '/uploadfile',
+        uploadButton = $('<button/>')
+            .addClass('btn btn-primary')
+            .attr('type','button')
+            .prop('disabled', true)
+            .text('准备...')
+            .on('click', function () {
+                var $this = $(this),
+                    data = $this.data();
+                $this
+                    .off('click')
+                    .text('正在上传...')
+                    .on('click', function () {
+                        $this.remove();
+                        data.abort();
+                    });
+                //提交或拒绝执行的操作
+                data.submit().always(function () {
+                    $this.remove();
+                });
+            });
+    $('#fileupload').fileupload({
+        url: url,
+        dataType: 'json',
+        autoUpload: false,
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png|wmv)$/i,
+        maxFileSize: 0,
+        // Enable image resizing, except for Android and Opera,
+        // which actually support image resizing, but fail to
+        // send Blob objects via XHR requests:
+        disableImageResize: /Android(?!.*Chrome)|Opera/
+            .test(window.navigator.userAgent),
+        previewMaxWidth: 100,
+        previewMaxHeight: 100,
+        previewCrop: true
+        //图片添加完成后触发的事件
+    }).on('fileuploadadd', function (e, data) {
+        data.context = $('<span/>').appendTo('#files');
+        $.each(data.files, function (index, file) {
+
+            var node = $('<span/>').text(file.name);
+            if (!index) {
+                node
+                    .append('<br>')
+                    .append(uploadButton.clone(true).data(data));
+            }
+            node.appendTo(data.context);
+
+        });
+        //当一个单独的文件处理队列结束触发(验证文件格式和大小)
+    }).on('fileuploadprocessalways', function (e, data) {
+        var index = data.index,
+            file = data.files[index],
+            node = $(data.context.children()[index]);
+        if (file.preview) {
+            node
+                .prepend('<br>')
+                .prepend(file.preview);
+        }
+        if (file.error) {
+            node
+                .append('<br>')
+                .append($('<span class="text-danger"/>').text(file.error));
+        }
+        if (index + 1 === data.files.length) {
+            data.context.find('button')
+                .text('上传')
+                .prop('disabled', !!data.files.error);
+        }
+
+        //显示上传进度条
+    }).on('fileuploadprogressall', function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        $('#progress .progress-bar').css(
+            'width',
+            progress + '%'
+        );
+        //上传请求成功时触发的回调函数
+    }).on('fileuploaddone', function (e, data) {
+        $.each(data.result.files, function (index, file) {
+            if (file.url) {
+                data.context = $('<p><span style="color:green;">上传成功</span></p>').appendTo('#files');
+                $('#video_src').val(file.name);
+
+            } else if (file.error) {
+                var error = $('<span class="text-danger"/>').text(file.error);
+                $(data.context.children()[index])
+                    .append('<br>')
+                    .append(error);
+            }
+        });
+        //上传请求失败时触发的回调函数
+    }).on('fileuploadfail', function (e, data) {
+        $.each(data.files, function (index) {
+            var error = $('<span class="text-danger"/>').text('File upload failed.');
+            $(data.context.children()[index])
+                .append('<br>')
+                .append(error);
+        });
+    }).prop('disabled', !$.support.fileInput)
+        .parent().addClass($.support.fileInput ? undefined : 'disabled');
+});
