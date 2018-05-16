fb0fdae913a3b5fb766aced1f32d2189fa3447d8
jkj007 <38996640+jkj007@users.noreply.github.com>
2018-05-16 13:32:16 +0800

    1

--- runtime/temp/9b4e65780b57b90e712ac04847b01fad.php
+++ runtime/temp/9b4e65780b57b90e712ac04847b01fad.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:59:"D:\phpStudy\WWW\dt./application/index\view\index\index.html";i:1526431589;s:35:"public/static/index/common/nav.html";i:1526431589;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:59:"D:\phpStudy\WWW\dt./application/index\view\index\index.html";i:1526431589;s:35:"public/static/index/common/nav.html";i:1526432281;}*/ ?>
  <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,79 +14,7 @@
     <header class="main bg-imgs home-1">
         <div class="container">
             <nav class="navbar" role="navigation">
-<<<<<<< HEAD
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="/index">
-                        <img id="index" src="/public/static/index/img/mylogo.png" style="position:relative;top:18px" width="120px" alt="eLearn" />
-                    </a>
-                </div>
-                <div class="collapse navbar-collapse">
-                <div class="navbar-right menu-main"> 
-                    <ul class="nav navbar-nav">
-                        <li><a href="/aboutus"><span>关于我们</span></a></li>
-                        <li class="dropdown">
-                            <a href="/public/static/index/#" class="dropdown-toggle" data-toggle="dropdown"><span>资源</span> <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
 
-                   
-
-                                <?php echo getNav(); ?>
-                            </ul>
-                        </li>
-                
-
-                        <li class="dropdown">
-                            <a href="/public/static/index/#" class="dropdown-toggle" data-toggle="dropdown"><span>项目概述</span> <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="nexus">投资者关系</a></li>
-                                <li><a href="abstracts">公司简介</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="contact"><span>联系我们</span></a></li>
-                    
-                         <li class="dropdown">
-                            <a href="/public/static/index/#" class="dropdown-toggle" data-toggle="dropdown"><span>相关知识</span> <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="/blog">店淘系</a></li>
-                              <!--   <li><a href="/pdd">拼多多</a></li>
-                                <li><a href="/jd">京东</a></li>
-                                <li><a href="/lazada">lazada</a></li> -->
-                            
-                         
-                            </ul>
-                        </li>
-
-                    <?php if(\think\Request::instance()->session('username.name') == null): ?>
-                    </ul>
-                    <a class="btn btn-theme navbar-btn btn-default sign-in" href="login">登陆</a>
-                    <a class="btn btn-theme navbar-btn btn-orange  sign-up" href="reg">注册</a>
-                    <?php else: ?>
-                    <li><a href="mycenter"><span>个人中心</span></a></li>
-                    </ul>
-                    <a class="btn btn-theme navbar-btn btn-default  sign-up" href="loginout">退出</a>
-                    <?php endif; ?>
-
-
-                     
-
-                        
-                      <!--   <li><a href="lt"><span>论坛</span></a></li> -->
-                      
-                    </ul>
-                  
-
-                   
-
-                </div>
-                </div>
-            </nav>
-=======
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
@@ -122,7 +50,7 @@
         </div>
     </div>
 </nav>
->>>>>>> f7b74474ff5ef15056d7492183385ef0e621225e
+
 
         </div>
     </header>
